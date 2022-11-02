27008ec773ebf108a1a5697b0c2347c2f57110f7
78Pavel78 <112005061+78Pavel78@users.noreply.github.com>
2022-11-02 15:12:11 +0300

    Add files via upload
    HomeWork

---
+++ variables.js
@@ -0,0 +1,17 @@
+
+// объявляем переменные
+let myAge = 100;
+let myBestFriendAge = 100;
+let myBirthYear = 1922;
+let thisYear = 2022;
+let dollarValue = 60;
+
+// задаем константы
+const myName = ' Pavel ';
+const myBestFriendName =' Peter ';
+const myCity = ' St-Petersburg ';
+const capitalRussia = ' Moscow ';
+const myDecription = ' mobile ';
+
+//выводим надпись
+console.log('Меня зовут'+ myName + ', мне ' +myAge+ ' года/лет. Я из города' + myCity +' и я'+ myDecription+'.');                                       
